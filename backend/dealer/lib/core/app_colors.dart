37caf54ab46a5b2f96867b2d1a1a0e5b3966a87d
karan <karan2005n@gmail.com>
2026-06-20 22:31:11 +0530

    hello

--- backend/dealer/lib/core/app_colors.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static const primary = Color(0xfff58220);
-  static const textDark = Color(0xff1E293B);
-  static const textLight = Color(0xff64748B);
-  static const border = Color(0xffE2E8F0);
-  static const white = Colors.white;
-}
